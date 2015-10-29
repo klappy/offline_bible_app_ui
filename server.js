35638e3d1dee9f432588f3f4c00332adb46ab4ea
uday <udayonrails@hotmail.com>
2015-10-29 15:28:47 +0530

    express version upgrade 3 to 4

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ var http = require('http');
 var path = path = require('path');
 var uuid = require('node-uuid');
 
+
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
+var json = require('json')
+
 var vhost = 'nodejsapp.local'
 var port     = process.env.PORT || 4000;
 var ip     = process.env.IP || "localhost";
@@ -19,36 +29,32 @@ var connection = require('./config/database')(mongoose);
 var models = require('./models/models')(connection);
 require('./config/passport')(passport,models); // pass passport for configuration
 
+// set up our express application
+app.set('port', port);
+app.use(logger('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.set('view engine', 'html'); // set up html for templating
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.session({ secret: 'keyboard cat' }));// persistent login sessions
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+//app.use(flash()); // use connect-flash for flash messages stored in session
 
-app.configure(function() {
-    // set up our express application
-    app.set('port', port);
-    app.use(express.logger('dev')); // log every request to the console
-    app.use(express.cookieParser()); // read cookies (needed for auth)
-    app.use(express.bodyParser()); // get information from html forms
-    app.set('view engine', 'html'); // set up html for templating
-    app.engine('.html', require('ejs').__express);
-    app.set('views', __dirname + '/views');
-    app.use(express.static(path.join(__dirname, 'public')));
-    //app.use(express.session({ secret: 'keyboard cat' }));// persistent login sessions
-    app.use(express.methodOverride());
-    app.use(express.json());
-    app.use(express.urlencoded());
-    //app.use(flash()); // use connect-flash for flash messages stored in session
-
-    //passport configuration
-    app.use(passport.initialize());
-    //app.use(passport.session());// persistent login sessions
-    //provagg
-    app.use(app.router); //init routing
-
-});
+//passport configuration
+app.use(passport.initialize());
+//app.use(passport.session());// persistent login sessions
 
 require('./app/routes.js')(app, passport,models); // load our routes and pass in our app and fully configured passport
 
 // development only
 if (app.get('env') === 'development') {
-    app.use(express.errorHandler());
+    app.use(errorHandler());
 };
 
 // production only
