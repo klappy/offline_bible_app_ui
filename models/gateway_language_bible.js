40512fc07bd5b206dfcfbede9fc68e7087da3620
rahul samaddar <rahul.samaddar@jktech.com>
2015-11-05 17:53:12 +0530

    api integration

---
+++ models/gateway_language_bible.js
@@ -0,0 +1,23 @@
+var Gateway_language_bible = {};
+var request = require('request');
+
+
+Gateway_language_bible.info =  function(authUser) {
+
+	var gateway_language_bible = {};
+
+	request("http://localhost:32781/api/bibles", function (error, response, body) {
+	
+		console.log(error);
+		console.log(body + "i am in model class");
+	
+		var resJson = JSON.parse(body);
+		gateway_language_bible = {bibleId:resJson['bibleId'], version:resJson['version'], language:resJson['language']};
+	
+	});
+	//var gateway_language_bible = {bibleId:'bibleId', version:'version', language:'hindi'};
+	return gateway_language_bible;
+}
+
+
+module.exports = Gateway_language_bible;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var Gateway_language_bible = require('../models/gateway_language_bible');
 
 module.exports = router
 
@@ -13,9 +14,16 @@ router.get('/home', function(req, res){
 /* GET Home Page */
 router.get('/', function(req, res){
   var authUser = req.session.user
-  return res.render('home', { user: User.info(authUser) });
+  return res.render('home', { gateway_language_bible: Gateway_language_bible.info(authUser) });
 });
 
-
+router.get( "/auth/loginCallback", function(req, res){
+  //var auth_token = res.params.token;
+  var sess = {
+    auth_token: res.params.token
+  }
+  app.use(session(sess));
+  
+})
 
 
